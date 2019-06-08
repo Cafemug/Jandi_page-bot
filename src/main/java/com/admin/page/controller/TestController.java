7b1c79557b937a0351f94baf9abbe4b0976e0d90
Jeon Ga Bin <hyanghope@naver.com>
2019-06-08 23:10:05 +0900

    :memo: Controller 골격 구현

--- src/main/java/com/admin/page/controller/TestController.java
+++ src/main/java/com/admin/page/controller/TestController.java
@@ -2,12 +2,22 @@ package com.admin.page.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping(value = "/hi")
-    public String sayHello() {
+    @PostMapping(value = "/user")
+    public String postUser() {
         return "hello world";
     }
+    @GetMapping(value = "/user")
+    public String getUser() {
+        return "hello world";
+    }
+    @GetMapping(value = "/commit")
+    public String getCommit() {
+        return "hello world";
+    }
+
 }
