594c6a8b40908a088940339dcdcb1c95da3f5a96
Jeon Ga Bin <hyanghope@naver.com>
2019-08-04 00:29:34 +0900

    #4 각자의 아이디로 커밋 나눠서 보여주기 예외처리

--- test_by_python/controller/get_name_commit.py
+++ test_by_python/controller/get_name_commit.py
@@ -7,20 +7,25 @@ from db_connect import mysql
 
 class GetNameCommit(Resource):
     def get(self, name):
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        query = "select * from crawler where nickname ='{name}'".format(name=name)
-        cursor.execute(query)
-        data = cursor.fetchone()
-        result =[]
-        temp ={}
-        temp["id"]=data[0]
-        temp["value"]= data[1]
-        print(temp)
-        result.append(temp)
-        print(result)
-        cursor.close()
-        conn.close()
+        names = ['컴공돌이', '또르', '복이', '뇸뇸', 'ㄷㄷ', '뇌가딴딴', '싸이클러', '1컴이', '레게힙합소년', '방탕성현단', '해피스마일', 'ccpo', '깃토리', '퐁퐁', '깃별', '하준', '맹코', '감동란', '현', '개발냄새', 'GIT촙오', '야옹', '개입', '펭귄']
+        if(not name in names):
+            result = [{'result' : "Not Found name"}]
+            return make_response(json.dumps(result, ensure_ascii=False))
+        else:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            query = "select * from crawler where nickname ='{name}'".format(name=name)
+            cursor.execute(query)
+            data = cursor.fetchone()
+            result =[]
+            temp ={}
+            temp["id"]=data[0]
+            temp["value"]= data[1]
+            print(temp)
+            result.append(temp)
+            print(result)
+            cursor.close()
+            conn.close()
         return make_response(json.dumps(result, ensure_ascii=False))
 
 
